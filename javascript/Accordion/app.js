d5ed6a1fdae0101eb16105a4658ded18a97dd119
antwolfe <muddywolfe77@gmail.com>
2022-05-18 12:03:45 -0500

    added toggle function to event listener

--- javascript/Accordion/app.js
+++ javascript/Accordion/app.js
@@ -1,36 +1,52 @@
 
 const arrows = document.querySelectorAll('.arrow-icon') //img.arrow-icon list
-
+const firstArrow = document.querySelector('.arrow-icon');
 // console.log(arrows);
 
 const answers = document.getElementsByClassName('answer hidden'); //p.answer.hidden list
 
-//answersArray = Array.from(answers); //turn HTMLcollection into array
+answersArray = Array.from(answers); //turn HTMLcollection into array
 // console.log(answers[0].textContent);
 
 // for each arrow in list, create an alert when the image is clicked
-arrows.forEach(arrow => {
-    arrow.addEventListener('click', arrowClicked);
-});
-
-function arrowClicked(e){
-    // show hidden answers
-    //if arrowclicked index matches answerhidden index, add answer under clicked arrow
-
-    
-
-}
 
 
+console.log(firstArrow);
 
-
-answersArray.forEach(answer => {
-    answer.addEventListener('click', arrowClicked);
+firstArrow.addEventListener('click', function(){
+    answers[0].classList.toggle('hidden');
 })
 
-function arrowClicked(e) {
-    console.log("Is this my arrow?");
-}
+
+
+
+
+// arrows.forEach((arrow) => {
+//     arrow.addEventListener('click', arrowClicked(arrow));
+// });
+
+// function arrowClicked(e) {
+//     if (arrow == arrows[0]) {
+//         console.log('yes');         
+//     } else {
+//         console.log("no");
+//     }
+// }
+
+
+// function arrowClicked(e){
+//     // show hidden answers
+//     //refactor: if arrowclicked index matches answerhidden index, add answer under clicked arrow
+
+// }
+
+
+
+
+// answersArray.forEach(answer => {
+//     answer.addEventListener('click', arrowClicked);
+// })
+
 
 
 //4. add shown 'answer hidden' class to event
