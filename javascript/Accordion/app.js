0232dd43129b342031d81b7184894aaa4c40ca4e
antwolfe <muddywolfe77@gmail.com>
2022-05-18 10:25:17 -0500

    add accordion and Form Validation files

---
+++ javascript/Accordion/app.js
@@ -0,0 +1,10 @@
+const section = document.getElementsByClassName('faq-item');
+
+console.log(section[0].lastElementChild.textContent);
+console.log(section[1].lastElementChild.textContent);
+console.log(section[2].lastElementChild.textContent);
+console.log(section[3].lastElementChild.textContent);
+console.log(section[4].lastElementChild.textContent);
+
+
+
