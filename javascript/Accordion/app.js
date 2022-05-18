74baf769c5e6c1aea1f7cd23febf84f93f06eb2d
antwolfe <muddywolfe77@gmail.com>
2022-05-18 10:41:14 -0500

    add HW steps in comments

--- javascript/Accordion/app.js
+++ javascript/Accordion/app.js
@@ -1,10 +1,22 @@
 const section = document.getElementsByClassName('faq-item');
 
-console.log(section[0].lastElementChild.textContent);
-console.log(section[1].lastElementChild.textContent);
-console.log(section[2].lastElementChild.textContent);
-console.log(section[3].lastElementChild.textContent);
-console.log(section[4].lastElementChild.textContent);
+// console.log(section[0].lastElementChild.textContent);
 
 
+// 1. define arrow to click
+
+//2. create simple event when arrow is clicked like an alert
+
+//3. find best route to show 'answer hidden' class
+
+//4. add shown 'answer hidden' class to event
+
+// 5. refactor: for each or map through section list adding onclick event for each arrow image?
+
+
+// arrow.addEventListener("click", onClick())
+
+// function onClick(e) {
+    
+// }
 
