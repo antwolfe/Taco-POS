1cfe4ccc9fa9543389c9269d6dd6c92b8803dcfa
antwolfe <muddywolfe77@gmail.com>
2022-05-18 16:55:14 -0500

    create search array from map method

--- javascript/Form Validation/app.js
+++ javascript/Form Validation/app.js
@@ -1,46 +1,42 @@
 // on click button[type = submit] -> first name should be valid -> if not: error
 
-//submitbutton.addEventListener('click', function)
 
-/* function() {
-    if (firstName == "") {
-        expose -> first-name-error
-    }
-}
-
-// will need a way to continue checking forms even if one pops an error.
-*/
-let searchName;
-let searchArray = [];
-const ids = document.querySelectorAll('input');
-
-for (let i = 0; i < ids; i++){
-    searchArray.push(ids[i].name);
-}
-console.log(searchArray);
-
-ids.forEach(id => {
-    searchArray.push(id.name);
-});
-
-console.log(searchArray);
-
-
-
+const ids = Array.from(document.querySelectorAll('input'));
 const submitBtn = document.querySelector('button');
-const firstName = document.querySelector('#first-name');
+const searchArray = ids.map(id => 
+                        {return `#${id.name}`;
+                    })
+
+// array of search ID's for all input fields
+
+
+
+
+// searchArray.forEach(inputTerm => {
+//     if (document.querySelector(`${inputTerm.value}`) == "") {
+//         // document.querySelector(`${inputTerm}-error`).style.visibility = 'visible';
+//         console.log("Do Something");
+//     }
+// });
+
+
+
+// const firstName = document.querySelector('#first-name');
 // console.log(firstName);
 
 submitBtn.addEventListener('click', validateForm)
 
 function validateForm(e) {
 
-    // switch()
-    if (firstName.value == ""){
-        document.querySelector('#first-name-error').style.visibility = 'visible';
-    } else {
-        console.log("what now?");
-    }
+    console.log(searchArray);
+  
+    // if (firstName.value == ""){
+    //     document.querySelector('#first-name-error').style.visibility = 'visible';
+    // } else {
+    //     console.log("what now?");
+    // }
     e.preventDefault();
 };
+
+// switch()
 
