23da938014430817535984ec954ac01a86065ffb
antwolfe <muddywolfe77@gmail.com>
2022-05-19 17:08:07 -0500

    refactor

--- javascript/Form Validation/app.js
+++ javascript/Form Validation/app.js
@@ -5,63 +5,47 @@ const ids = Array.from(document.querySelectorAll('input'));
 const searchArray = ids.map(id => {
     return `#${id.name}`;
 });
-const messageArray = ["Email cannot be empty", "Looks like this is not a valid email"]
 
-// console.log(ids);
-// console.log(searchArray);
-
-form.addEventListener('submit', validateForm)
-
+form.addEventListener('submit', validateForm);
 
 function validateForm(e) {
-
     e.preventDefault();
-    
-    resetErrors(searchArray);
-    
+    resetEmailError();
+
     for (let i = 0; i < searchArray.length; i++) {
+
         let inputField = document.querySelector(searchArray[i]).value;
         let currentId = searchArray[i];
 
         if (inputField == "") {
-
             document.querySelector(currentId + "-error").style.visibility = "visible";
-
-
         } else {
-
             if (currentId == "#email" && !validateEmailInput(inputField)) {
-
-                console.log(document.getElementById("email-error"));
-                document.getElementById("email-error").textContent = "Looks like this is not a valid email";
-                document.getElementById('email-error').style.visibility = "visible";
-            
-
+                displayInvalidErrorMessage();
             } else {
-
-            document.querySelector(currentId + "-error").style.visibility = "hidden";
+                document.querySelector(currentId + "-error").style.visibility = "hidden";
             }
-
         }
-
     }
 }
 
-
 function validateEmailInput(email) {
+    // validates email with regex
+
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         return (true)
     }
-}
+};
 
-function resetErrors(searchArray) {
-    
+function displayInvalidErrorMessage() {
+    // changes "#email-error" textContent and changes class visibility
 
-    document.querySelector("#email-error").textContent = "Email cannot be empty"
-    
-}
+    document.getElementById("email-error").textContent = "Looks like this is not a valid email";
+    document.getElementById('email-error').style.visibility = "visible";
+};
 
+function resetEmailError() {
+    // resets "#email-error" to original message
 
-// break out validate email function
-// have a reset errors function
-// clean up code
+    document.querySelector("#email-error").textContent = "Email cannot be empty";
+};
