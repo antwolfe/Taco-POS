a486684c199dcde8c3f9a1e7dfe6fe8f2285e01a
antwolfe <muddywolfe77@gmail.com>
2022-05-19 16:51:19 -0500

    add email message reset

--- javascript/Form Validation/app.js
+++ javascript/Form Validation/app.js
@@ -7,31 +7,33 @@ const searchArray = ids.map(id => {
 });
 const messageArray = ["Email cannot be empty", "Looks like this is not a valid email"]
 
-
+// console.log(ids);
+// console.log(searchArray);
 
 form.addEventListener('submit', validateForm)
 
+
 function validateForm(e) {
 
+    e.preventDefault();
+    
+    resetErrors(searchArray);
+    
     for (let i = 0; i < searchArray.length; i++) {
-        let inputField = document.querySelector(searchArray[i]).value
+        let inputField = document.querySelector(searchArray[i]).value;
         let currentId = searchArray[i];
 
         if (inputField == "") {
 
             document.querySelector(currentId + "-error").style.visibility = "visible";
 
-        // } else if (inputField == "" && currentId == "#email") {
 
-        //     document.getElementById('email-error').innerText = "Email cannot be empty"
-        //     document.getElementById('email-error').style.visibility = "visible";
+        } else {
 
-        } else if (inputField != "") {
-
-            if (currentId == "#email" && !validateEmail(inputField)) {
+            if (currentId == "#email" && !validateEmailInput(inputField)) {
 
                 console.log(document.getElementById("email-error"));
-                document.getElementById("email-error").innerText = "Looks like this is not a valid email";
+                document.getElementById("email-error").textContent = "Looks like this is not a valid email";
                 document.getElementById('email-error').style.visibility = "visible";
             
 
@@ -41,19 +43,25 @@ function validateForm(e) {
             }
 
         }
-        e.preventDefault();
 
     }
 }
 
 
-function validateEmail(email) {
+function validateEmailInput(email) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         return (true)
     }
 }
 
-// switch statement refactor?
-// could maybe use a while loop
-// variable refactor?
- // clean up code
+function resetErrors(searchArray) {
+    
+
+    document.querySelector("#email-error").textContent = "Email cannot be empty"
+    
+}
+
+
+// break out validate email function
+// have a reset errors function
+// clean up code
