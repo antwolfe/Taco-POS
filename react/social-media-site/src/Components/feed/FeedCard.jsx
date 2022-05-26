ca71f08051ddd416f41ab006f696aaa206dfdbd1
antwolfe <muddywolfe77@gmail.com>
2022-05-25 22:07:05 -0500

    work on comment accordion

--- react/social-media-site/src/Components/feed/FeedCard.jsx
+++ react/social-media-site/src/Components/feed/FeedCard.jsx
@@ -20,17 +20,29 @@ export default function FeedCard(props) {
           <img src={props.image} />
         </div>
         <div className="content">
-          <p className="description">{props.description}</p>
+          <p className="description" style={{ padding: 2 }}>
+            {props.description}
+          </p>
           <span className="right floated">
             <i className="heart outline like icon"></i>
             {props.likeCount}
           </span>
           <div className="ui accordion">
             <i
-              style={{}}
+              style={{ cursor: "pointer" }}
               className="comment icon"
               onClick={() => {
+                document
+                  .querySelector("#show-comments")
+                  .classList.toggle("visible")(
+                  <p id="show-comments" class="transition hidden">
+                    A dog is a type of domesticated animal. Known for its
+                    loyalty and faithfulness, it can be found as a welcome guest
+                    in many households across the world.
+                  </p>
+                );
                 console.log("hello");
+                console.log("goodbye");
               }}
             ></i>
             3 comments
