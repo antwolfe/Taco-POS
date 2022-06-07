1f131bb25ca8417cc43ff7fabba340896aed7636
antwolfe <muddywolfe77@gmail.com>
2022-06-07 00:49:30 -0500

    add all images

--- capstone1/ecommerce_store/src/Components/InventoryItem.jsx
+++ capstone1/ecommerce_store/src/Components/InventoryItem.jsx
@@ -17,8 +17,12 @@ export default function InventoryItem({
         <div className="preview-item">
           <img className="book-cover" src={product.cover} alt="book cover" />
           <span>
-            <h2 className="book-title">{product.title}</h2>
-            <h5 className="book-price">${product.price}</h5>
+            <h2 className="book-title">Title: {product.title}</h2>
+            <h5 className="book-id">ID#: {product.id}</h5>
+            <h5 className="book-year">In Stock? {product.inventory}</h5>
+            <h5 className="book-price">Selling Price: ${product.price}</h5>
+            <h5 className="book-price">ISBN: {product.price}</h5>
+
             <a href="/">
               <div className="counter">
                 <Button
