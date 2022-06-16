bf94327168f9e8f148f398c2dcca4ae1ed7bc184
antwolfe <muddywolfe77@gmail.com>
2022-06-15 22:43:03 -0500

    add side class. add side object/string to order class

---
+++ java/tektacos/src/main/java/com/teksystems/bootcamp/Side.java
@@ -0,0 +1,17 @@
+package com.teksystems.bootcamp;
+
+public class Side {
+    String side;
+
+    public Side(String side) {
+        this.side = side;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+}
