90bb3d332a98ae93bfac2ba1255fd02b75d47c1e
antwolfe <muddywolfe77@gmail.com>
2022-06-20 10:12:44 -0500

    finish input validation, add item confirmation output

--- java/tektacos/src/main/java/com/teksystems/bootcamp/TakeOrder.java
+++ java/tektacos/src/main/java/com/teksystems/bootcamp/TakeOrder.java
@@ -5,17 +5,27 @@ import java.util.Scanner;
 public interface TakeOrder {
     // user input handling
 
-    default int getUserInput() { //VALIDATION
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Choose a number: ");
-        return scanner.nextInt();
+    default int getUserInput(int max_num) { //VALIDATION
+        boolean isValidInput = false;
+        int response = 0;
+        while (!isValidInput) {
+            System.out.print("Choose a number: ");
+            Scanner scanner = new Scanner(System.in);
+            response =  scanner.nextInt();
+            if (response <= max_num) {
+                isValidInput = true;
+            } else {
+                System.out.println("Incorrect response, please try again.");
+            }
+        }
+        return response;
     }
 
     default Order fromMainMenu(Order theOrder) {
         boolean completedOrder = false;
         while (!completedOrder) {
             Menu.mainMenu();
-            int choice = getUserInput();
+            int choice = getUserInput(5);
 
             switch (choice) {
                 case 1:
@@ -59,7 +69,7 @@ public interface TakeOrder {
     default void fromEntreeMenu(Order theOrder) {
         Menu.entreeMenu();
         Taco chosenTaco = null;
-        switch (theOrder.getUserInput()) {
+        switch (theOrder.getUserInput(4)) {
             case 1:
                 chosenTaco = new BasicTaco();
                 break;
@@ -81,22 +91,25 @@ public interface TakeOrder {
 
     default void fromTortillaMenu(Order theOrder, Taco theTaco) {
         Menu.tortillaMenu();
-        int choice = theOrder.getUserInput();
+        int choice = theOrder.getUserInput(Tortillas.values().length + 1);
         if (choice != 2 && (!theTaco.getTortilla().equals(Tortillas.CORN))) {
             theTaco.setTortilla(Tortillas.FLOUR);
+            System.out.println("You chose the flour tortilla");
         } else {
             theTaco.setTortilla(Tortillas.CORN);
+            System.out.println("You chose the corn tortilla");
         }
         theOrder.fromProteinMenu(theOrder, theTaco);
     }
 
     default void fromProteinMenu(Order theOrder, Taco theTaco) {
         Menu.proteinMenu();
-        int choice = theOrder.getUserInput();
         Proteins[] proteins = Proteins.values();
+        int choice = theOrder.getUserInput(proteins.length);
         for (Proteins protein : proteins) {
             if (proteins[choice - 1] == protein) {
                 theTaco.setProtein(proteins[choice - 1]);
+                System.out.println("You added " + proteins[choice - 1].getDisplayName());
             }
         }
         theOrder.fromToppingMenu(theOrder, theTaco);
@@ -106,10 +119,11 @@ public interface TakeOrder {
         Toppings[] toppings = Toppings.values();
         while (theTaco.getToppingList().size() < theTaco.toppingLimit) {
             Menu.toppingMenu();
-            int choice = theOrder.getUserInput();
+            int choice = theOrder.getUserInput(toppings.length);
             for (Toppings topping : toppings) {
                 if (toppings[choice - 1] == topping) {
                     theTaco.addTopping(toppings[choice - 1]);
+                    System.out.println("You added " + toppings[choice - 1].getDisplayName());
                 }
             }
         }
@@ -118,22 +132,24 @@ public interface TakeOrder {
 
     default void fromSideMenu(Order theOrder) {
         Menu.sideMenu();
-        int choice = theOrder.getUserInput();
         Sides[] sides = Sides.values();
+        int choice = theOrder.getUserInput(sides.length);
         for (Sides side : sides) {
             if (sides[choice - 1] == side) {
                 theOrder.addItemToOrder(sides[choice - 1]);
+                System.out.println("You added " + sides[choice - 1].getDisplayName());
             }
         }
     }
 
     default void fromDrinksMenu(Order theOrder) {
         Menu.drinkMenu();
-        int choice = theOrder.getUserInput();
         Drinks[] drinks = Drinks.values();
+        int choice = theOrder.getUserInput(drinks.length);
         for (Drinks drink : drinks) {
             if (drinks[choice - 1] == drink) {
                 theOrder.addItemToOrder(drinks[choice - 1]);
+                System.out.println("You added " + drinks[choice - 1].getDisplayName());
             }
         }
     }
