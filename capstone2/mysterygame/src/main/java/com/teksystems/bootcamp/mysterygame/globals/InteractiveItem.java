bf7bcf52aa155640bdebc7a472f692ad6720dada
antwolfe <muddywolfe77@gmail.com>
2022-06-27 22:45:23 -0500

    fill in clues with interactive items

--- capstone2/mysterygame/src/main/java/com/teksystems/bootcamp/mysterygame/globals/InteractiveItem.java
+++ capstone2/mysterygame/src/main/java/com/teksystems/bootcamp/mysterygame/globals/InteractiveItem.java
@@ -1,20 +1,24 @@
 package com.teksystems.bootcamp.mysterygame.globals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public enum InteractiveItem { // TODO: use description as help for finding clues
 
-    TRASHCAN("You know what they say about another's trash...", ),
-    DESK("A big desk with many drawers. There is a computer on top"),
-    COMPUTER("A device for searching the internet"),
-    OVEN("There seems to be some food in there"),
-    COUNTER("You see a colorful magazine scattered amongst junk mail"),
-    MAT("Weird marks on the mat. Looks like someone tracked in some dirt"),
-    FRIDGE("You open the fridge and see lots of seasoned chicken ready for the grill"),
-    BBQ_PIT("Fixed in small holes in the dirt. Lots of coals burning"),
+    TRASHCAN("You know what they say about another's trash...", Clue.LOTTERY_TICKET),
+    DESK("A big desk with many drawers. There is a computer on top", Clue.DOG_TOY),
+    COMPUTER("A device for searching the internet", Clue.GOOGLE_RESULTS),
+    OVEN("There seems to be some food in there", Clue.CHICKEN),
+    COUNTER("You see a colorful magazine scattered amongst junk mail", Clue.ART_MAGAZINE),
+    MAT("Weird marks on the mat. Looks like someone tracked in some dirt", Clue.MUD_STAINS),
+    FRIDGE("You open the fridge and see lots of seasoned chicken ready for the grill", Clue.DOGFOOD),
+    BBQ_PIT("Fixed in small holes in the dirt. Lots of coals burning", Clue.CHICKEN),
     ;
     private final String description;
+    private final Clue clue;
 
 
-    InteractiveItem(String description) {
+    InteractiveItem(String description, Clue clue ) {
         this.description = description;
         this.clue = clue;
     }
