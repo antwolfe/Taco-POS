1f328e13ba24d92df7e2eb4abf690414973f07a0
antwolfe <muddywolfe77@gmail.com>
2022-06-29 18:15:38 -0500

    transfer validation methods to InputValidator class

--- capstone2/mysterygame/src/main/java/com/teksystems/bootcamp/mysterygame/ProcessInput.java
+++ capstone2/mysterygame/src/main/java/com/teksystems/bootcamp/mysterygame/ProcessInput.java
@@ -14,64 +14,12 @@ import static com.teksystems.bootcamp.mysterygame.Game.player;
 
 public class ProcessInput {
 
-    private static final InteractiveItem[] itemList = InteractiveItem.values();
-    static final String[] commandList = {"take", "go", "look", "examine", "x"};
-    static final String[] oneWordList = {"solve", "help", "inventory"};
-
-    private static boolean isValidVerb(String word) {
-        for (String command : commandList) {
-            if (word.equals(command)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isValidItem(String word) {
-        for (InteractiveItem item : itemList) {
-            if (!word.equals(item.toString())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isValidDirection(String word) {
-        for (Direction direction : Direction.values()) {
-            if (word.equals(direction.toString())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected static boolean isValidCommand(String words) {
-        String[] arrWords = words.toLowerCase().split(" ");
-        if (arrWords.length != 2) {
-            System.out.println("Please use two words");
-            return false;
-        }
-        if (isValidVerb(arrWords[0]) && isValidItem(arrWords[1])) {
-            return true;
-        }
-        if (isValidVerb(arrWords[0]) && isValidDirection(arrWords[1])) {
-            return true;
-        }
-        System.out.println("not a valid command");
-        return false;
-    }
-
-    void helpMenu() {
-        // TODO: help keyword in game
-    }
-
     protected static String processCommands(String words) {
 
         if (words.equals("help me")) {
             Game.showHelpMenu();
         }
-        else if (isValidCommand(words)) {
-            System.out.println("ok");
+        else if (InputValidator.isValidCommand(words)) {
             String[] arrWords = words.toLowerCase().split(" ");
             String verb = arrWords[0];
             String noun = arrWords[1];
