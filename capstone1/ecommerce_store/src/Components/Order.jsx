b94c49ce9a958cd4c7ec7a50510ae474f65f7b18
antwolfe <muddywolfe77@gmail.com>
2022-06-30 19:43:43 -0500

    add files to capstone 2, delete some from local

---
+++ capstone1/ecommerce_store/src/Components/Order.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Order() {
+  return (
+    <div>
+      <card>
+        <h1>Your Order Has Been Submitted! </h1>
+      </card>
+    </div>
+  );
+}
