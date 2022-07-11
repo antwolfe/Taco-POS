c3c53a3f8d60c74f8ab45c19dcae32d6d6664504
antwolfe <muddywolfe77@gmail.com>
2022-07-11 17:00:32 -0500

    continue template

--- design_patterns/design_patterns/src/main/java/com/teksystems/bootcamp/template/Template.java
+++ design_patterns/design_patterns/src/main/java/com/teksystems/bootcamp/template/Template.java
@@ -5,17 +5,16 @@ public abstract class Template {
     final void tellMeAStory() {
         System.out.println(getDay() + " I had " + getFood() + " for " + getMealTime() + " as I was " + getVerb() + " on a " + getFurnitureObject());
         System.out.println("The " + getFood() + "was " + getAdjective() + ", " + getAdjective() + ", and " + getAdjective() + ".");
-        System.out.println("As I was looking out of the window,  I saw a gray cat make it's way into the alley.");
-        System.out.println("The dark alley was littered with bread and fat birds.");
-        System.out.println("The cat was so surprised at the amount of birds that it attacked all the birds at once.");
-        System.out.println("It almost worked! But one of the birds flew up and grabbed a rickety streetlamp that came crashing down onto the cats head.");
+        System.out.println("As I was " + getVerb() + "on the " + getFurnitureObject() + ", I saw a " + getAdjective() + " " + getAnimal() + " make it's way into the " + getPlace());
+        System.out.println("The " + getAdjective() + " " + getPlace() + " was littered with " + getPluralNoun() + " and " + getAdjective() + " " + getAnimals() + ".");
+        System.out.println("The " + getAnimal() + " was so " + getAdjective() + " at the amount of " + getAnimals() + " that it attacked all the " + getAnimals() + " at once.");
+        System.out.println("It almost worked! But one of the " + getAnimals() + " " + getVerb() + " up and grabbed a " + getAdjective() + " " + getObject() + " that came crashing down onto the " + getAnimal() + "'s head.");
         System.out.println("The cat fainted.");
         System.out.println("I got up and wiped my face with a napkin and finished my lunch.");
     }
 
-    String getDay() {
 
-    }
+    String getDay() {}
 
     String getFood() {}
 
@@ -35,5 +34,8 @@ public abstract class Template {
 
     String getAnimal() {}
 
+    String getPluralNoun() {}
+
+    String getAnimals() {}
 
 }
