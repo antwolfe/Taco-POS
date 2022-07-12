8f97f31a77bcbf590859d1eefc1d64d3891a3184
antwolfe <muddywolfe77@gmail.com>
2022-07-12 09:33:25 -0500

    fix factory test

--- design_patterns/design_patterns/src/test/java/com/teksystems/bootcamp/factories/SantasFactoryTest.java
+++ design_patterns/design_patterns/src/test/java/com/teksystems/bootcamp/factories/SantasFactoryTest.java
@@ -7,36 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SantasFactoryTest {
 
-    SantasFactory factory = new SantasFactory();
-    Gift gift;
-    String naughty = "naughty";
-    String nice = "nice";
-
-    @Test
-    public void givenNiceMakeGiftReturnsToy() {
-        SantasFactory factory = new SantasFactory();
-        String nice = "nice";
-        Gift expected = new Toy();
-        Gift actual = factory.makeGift(nice);
-        assertEquals(expected, actual);
-        //  need to compare same object? different assert?
-    }
-
-    /**
-     * Method under test: {@link SantasFactory#makeGift(String)}
-     */
     @Test
     void testMakeGift() {
-        assertEquals(" gets coal!", (new SantasFactory()).makeGift("Status").getGift());
-        assertEquals(" gets a toy!", (new SantasFactory()).makeGift((String) "nice").getGift());
-    }
-
-    @Test
-    void givenNaughtyMakeGiftReturnsCoal() {
+        //given
         SantasFactory factory = new SantasFactory();
         String naughty = "naughty";
-        Gift expected = new Coal();
-        Gift actual = factory.makeGift(naughty);
-        assertEquals(expected, actual);
+        String nice = "nice";
+
+        //when
+        String actual = factory.makeGift(naughty).getGift();
+        String actual2 = factory.makeGift(nice).getGift();
+
+        //then
+        assertEquals(" gets coal!", actual);
+        assertEquals(" gets a toy!", actual2);
     }
 }
